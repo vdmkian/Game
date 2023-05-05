7d1fb3f3c1cc96ecd507da6aa20b07b6d60d05dc
Kian_van_der_Meer <kvanderm@ucsc.edu>
2023-05-05 14:57:53 -0700

    Changes to be committed: 	new file:   assets/BDGame.png 	new file:   assets/BDMenu.png 	new file:   assets/DBStudios.png 	new file:   assets/Doodlebob.mp3 	new file:   assets/Loading.mp3 	new file:   index.html 	new file:   lib/phaser.js 	new file:   readme.md 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Scene1'});
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('BDGame', 'BDGame.png');
+        this.load.audio('Doodlebob', ['Doodlebob.mp3']);
+    }
+    create(){
+        //var sound = this.sound.add('Loading');
+        this.sound.play('Doodlebob');
+        var game = this.add.image(300,300,'BDGame');
+        game.setScale(600/game.height,600/game.width);
+        this.graphics = this.add.graphics();
+        // add shapes
+        this.graphics.fillStyle(0xff0000, 1); //color, opacity
+        this.graphics.fillCircle(282,440,3);   //x, y, radius
+        this.graphics.fillCircle(265,460,3);   //x, y, radius
+        this.graphics.fillCircle(285,465,3);   //x, y, radius
+
+        this.cameras.main.once('camerafadeincomplete', function (camera) {
+            camera.fadeOut(6000);
+        });
+
+        this.cameras.main.fadeIn(6000);
+
+        this.input.once('pointerdown', function (event)
+        {
+            console.log('From SceneA to SceneB');
+
+            this.scene.start('Scene2');
+
+        },this);
+
+    }
+    update(){}
+}
+
+class Scene2 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Scene2'});
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('DBStudios', 'DBStudios.png');
+        this.load.audio('Loading', ['Loading.mp3']);
+    }
+    create(){
+        this.sound.play('Loading');
+        var loading = this.add.image(300,300,'DBStudios');
+        loading.setScale(600/loading.height,600/loading.width);
+
+        this.cameras.main.once('camerafadeincomplete', function (camera) {
+            camera.fadeOut(6000);
+        });
+
+        this.cameras.main.fadeIn(6000);
+
+        this.input.once('pointerdown', function (event)
+        {
+            console.log('From SceneB to SceneC');
+
+            this.scene.start('Scene3');
+
+        },this);
+
+    }
+    update(){}
+}
+
+class Scene3 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Scene3'});
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('BDMenu', 'BDMenu.png');
+    }
+    create(){
+        var menu = this.add.image(-300,300,'BDMenu');
+        menu.setScale(600/menu.height,600/menu.width);
+
+        this.tweens.add({
+            targets: menu,
+            x: 300,
+            duration: 2000,
+            repeat: 0,
+            ease: 'cubic.in'
+        });
+    }
+    update(){}
+}
+
+let config = {
+    type: Phaser.WEBGL,
+    width: 600,
+    height: 600,
+    backgroundColor: 0xFFFFFF,
+    scene: [Scene1, Scene2, Scene3],
+}
+
+let game = new Phaser.Game(config);
